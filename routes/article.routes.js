b86b521f38de8be58dc29c8f30605cd44024a331
Naufalnurfahriza <123190024@student.upnyk.ac.id>
2025-03-17 16:59:47 +0700

    update comment

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', articleController.getAllArticles);
 router.get('/:id', articleController.getArticleById);
 
-// Protected routes
+// Protected routes -admin only
 router.post('/', verifyToken, articleController.createArticle);
 router.put('/:id', verifyToken, articleController.updateArticle);
 router.delete('/:id', verifyToken, articleController.deleteArticle);
